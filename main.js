8ffaa822bd1881b56793961949dcd1210f11cd2c
rudy.prihantoro@xtremax.com <rudy.prihantoro@xtremax.com>
2023-05-16 10:19:17 +0700

    refactor for cleaner implmentation

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const API_CD_URL =
   "https://api.nimvio.com/cda/rest/v1/Project_2eb07e5e-2e66-4cba-b369-d8d597fb7e81/contents";
 
+// Fetch json data from Nimvio API CD and return it
 async function logJSONData() {
   const response = await fetch(API_CD_URL);
   const jsonData = await response.json();
 
-  processData(jsonData);
+  return jsonData;
 }
 
 // Processes the retrieved data and displays it on the page.
@@ -20,4 +21,11 @@ function processData(response) {
   document.getElementById("banner").src = picture;
 }
 
-logJSONData();
+// Render function
+async function render() {
+  const data = await logJSONData();
+  processData(data);
+}
+
+// Call render function
+render();
